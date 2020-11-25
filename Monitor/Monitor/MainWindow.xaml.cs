89d6259956b71f9f6b80e31d3b235e82e04f4709
maksatbek1998 <maksat.140298.kg@gmail.com>
2020-11-25 14:22:58 +0600

    Maksat

--- Monitor/Monitor/MainWindow.xaml.cs
+++ Monitor/Monitor/MainWindow.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAnimatedGif;
 
 namespace Monitor
 {
@@ -23,6 +25,42 @@ namespace Monitor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Personto_to_walk()
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("Image/GIF/Person.gif", UriKind.Relative);
+            image.EndInit();
+            ImageBehavior.SetAnimatedSource(imageAwesome, image);
+            ImageBehavior.SetRepeatBehavior(imageAwesome, new System.Windows.Media.Animation.RepeatBehavior(20));
+        }
+
+        public void Animation_For_Video()
+        {
+            if (GridWidth.Width==600)
+            {
+                DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+                myDoubleAnimation.From = GridWidth.Width;
+                myDoubleAnimation.To = 0;
+                myDoubleAnimation.Duration = TimeSpan.FromMilliseconds(500);
+                GridWidth.BeginAnimation(Grid.WidthProperty, myDoubleAnimation);
+            }
+            else
+            {
+                DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+                myDoubleAnimation.From = 0;
+                myDoubleAnimation.To = 600;
+                myDoubleAnimation.Duration = TimeSpan.FromMilliseconds(500);
+                GridWidth.BeginAnimation(Grid.WidthProperty, myDoubleAnimation);
+            }
+
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Personto_to_walk();
         }
     }
 }
---
+++ Monitor/Monitor/Message.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Monitor
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : Window
+    {
+        public Message(string klient, string okno)
+        {
+            InitializeComponent();
+            KLIENT.Text = klient.ToString();
+            OKNO.Text = okno.ToString();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Time;
+            timer.Start();
+        }
+        public void Time(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
