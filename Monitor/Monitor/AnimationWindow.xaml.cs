241537c33a0d8ccb7dfb8c2092bf606b4222e4d6
daniyarabdulhamidov@outlook.com <daniyarabdulhamidov@outlook.com>
2020-12-29 06:46:44 +0300

    ы

---
+++ Monitor/Monitor/AnimationWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monitor
+{
+    /// <summary>
+    /// Логика взаимодействия для AnimationWindow.xaml
+    /// </summary>
+    public partial class AnimationWindow : Window
+    {
+        public AnimationWindow()
+        {
+            InitializeComponent();
+            AnimatedText();
+        }
+        private void AnimatedText()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 1800;
+            da.To = -1900;
+            da.Duration = TimeSpan.FromSeconds(30);
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            anim.BeginAnimation(Canvas.LeftProperty, da);
+            anim.Text = "Нужно реализовать бегущую строку. Вроде как задача проще не придумаешь, но нарвался на косяк, который не могу разрулить уже два дня, походу все с ним сталкиваются, но адекватного решения в гуге так и не нашел.При запуске строка идет нормально, слегка подергивается, но вполне приемлемо, через десять минут беганий строка начинает дергаться сильнее, через пол часа дергается уже конкретно и давит на глаза. При этом в диспетчере наблюдаю что со временем медленно растет загрузка проца. Увеличил частоту кадров, стало немного лучше, но в принципе та же хрень :(Кто поборол эту проблему, выручайте! Весь проект уже написан, все отлично работает, а из - за этой мелочи никак сдать не могу :(";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desctop = System.Windows.SystemParameters.WorkArea;
+            this.Left = 0;
+            this.Top = desctop.Bottom - (this.Height);
+        }
+    }
+}
---
+++ Monitor/Monitor/Models/Naudio.cs
@@ -0,0 +1,290 @@
+﻿using System.Media;
+
+namespace Monitor.Models
+{
+    public static class Naudio
+    {    
+        public static void MusicPlay(string numSound,string operSound , string Lang)
+        {
+            try
+            {
+                if (Lang == "RU")
+                {
+
+                    SoundPlayer Sound = new SoundPlayer(@"C:\rsk\sound\ru\ЭКГ.wav");
+                    Sound.PlaySync();
+                    char suffix;
+                    suffix = numSound[0];
+                    Sound.SoundLocation = @"C:\rsk\sound\ru\" + suffix + ".wav";
+                    Sound.PlaySync();
+                    numSound = numSound.Substring(1);
+                    if (int.Parse(numSound) <= 100)
+                    {
+                        Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSound + ".wav";
+                        Sound.PlaySync();
+                    }
+                    else if (int.Parse(numSound) > 100 && int.Parse(numSound) < 1000)
+                    {
+                        char sto = numSound[0];
+                        string stosh = sto + "00";
+                        Sound.SoundLocation = @"C:\rsk\sound\ru\" + stosh + ".wav";
+                        Sound.PlaySync();
+
+                        if (numSound[1] == '0')
+                        {
+                            Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSound[2] + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string numSoundSub = numSound.Substring(1);
+                            Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSoundSub + ".wav";
+                            Sound.PlaySync();
+                        }
+                    }
+                    else if (int.Parse(numSound) >= 1000 && int.Parse(numSound) < 10000)
+                    {
+                        if (numSound[1] == '0' && numSound[2] == '0' && numSound[3] == '0')
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\ru\" + stosh + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\ru\" + stosh + ".wav";
+                            Sound.PlaySync();
+
+
+                            if (numSound[1] == '0' && numSound[2] == '0')
+                            {
+                                Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSound[3] + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else if (numSound[1] == '0' && numSound[2] != '0')
+                            {
+                                string stos = numSound.Substring(2);
+                                Sound.SoundLocation = @"C:\rsk\sound\ru\" + stos + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else
+                            {
+                                char sto = numSound[1];
+                                string stoshi = sto + "00";
+                                Sound.SoundLocation = @"C:\rsk\sound\ru\" + stoshi + ".wav";
+                                Sound.PlaySync();
+                                if (numSound[2] == '0')
+                                {
+                                    Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSound[3] + ".wav";
+                                    Sound.PlaySync();
+                                }
+                                else
+                                {
+                                    string numSoundSub = numSound.Substring(2);
+                                    Sound.SoundLocation = @"C:\rsk\sound\ru\" + numSoundSub + ".wav";
+                                    Sound.PlaySync();
+                                }
+                            }
+                        }
+                    }
+                    Sound.SoundLocation = @"C:\rsk\sound\ru\пройдите_в_отдел.wav";
+                    Sound.PlaySync();
+                    Sound.SoundLocation = @"C:\rsk\sound\ru\" + operSound + ".wav";
+                    Sound.PlaySync();
+                }
+                else if (Lang == "KG")
+                {
+                    SoundPlayer Sound = new SoundPlayer(@"C:\rsk\sound\kg\А.wav");
+                    Sound.PlaySync();
+                    char suffix;
+                    suffix = numSound[0];
+                    Sound.SoundLocation = @"C:\rsk\sound\kg\" + suffix + ".wav";
+                    Sound.PlaySync();
+                    numSound = numSound.Substring(1);
+                    if (int.Parse(numSound) <= 100)
+                    {
+                        Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSound + ".wav";
+                        Sound.PlaySync();
+                    }
+                    else if (int.Parse(numSound) > 100 && int.Parse(numSound) < 1000)
+                    {
+                        char sto = numSound[0];
+                        string stosh = sto + "00";
+                        Sound.SoundLocation = @"C:\rsk\sound\kg\" + stosh + ".wav";
+                        Sound.PlaySync();
+
+                        if (numSound[1] == '0')
+                        {
+                            Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSound[2] + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string numSoundSub = numSound.Substring(1);
+                            Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSoundSub + ".wav";
+                            Sound.PlaySync();
+                        }
+                    }
+                    else if (int.Parse(numSound) >= 1000 && int.Parse(numSound) < 10000)
+                    {
+                        if (numSound[1] == '0' && numSound[2] == '0' && numSound[3] == '0')
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\kg\" + stosh + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\kg\" + stosh + ".wav";
+                            Sound.PlaySync();
+
+
+                            if (numSound[1] == '0' && numSound[2] == '0')
+                            {
+                                Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSound[3] + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else if (numSound[1] == '0' && numSound[2] != '0')
+                            {
+                                string stos = numSound.Substring(2);
+                                Sound.SoundLocation = @"C:\rsk\sound\kg\" + stos + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else
+                            {
+                                char sto = numSound[1];
+                                string stoshi = sto + "00";
+                                Sound.SoundLocation = @"C:\rsk\sound\kg\" + stoshi + ".wav";
+                                Sound.PlaySync();
+                                if (numSound[2] == '0')
+                                {
+                                    Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSound[3] + ".wav";
+                                    Sound.PlaySync();
+                                }
+                                else
+                                {
+                                    string numSoundSub = numSound.Substring(2);
+                                    Sound.SoundLocation = @"C:\rsk\sound\kg\" + numSoundSub + ".wav";
+                                    Sound.PlaySync();
+                                }
+                            }
+                        }
+                    }
+                    Sound.SoundLocation = @"C:\rsk\sound\kg\ЭКГ.wav";
+                    Sound.PlaySync();
+                    Sound.SoundLocation = @"C:\rsk\sound\kg\" + operSound + ".wav";
+                    Sound.PlaySync();
+                    Sound.SoundLocation = @"C:\rsk\sound\kg\РегистрРегистр.wav";
+                    Sound.PlaySync();
+                }
+                else if (Lang == "EN")
+                {
+                    SoundPlayer Sound = new SoundPlayer(@"C:\rsk\sound\en\A.wav");
+                    Sound.PlaySync();
+                    char suffix;
+                    suffix = numSound[0];
+                    Sound.SoundLocation = @"C:\rsk\sound\en\" + suffix + ".wav";
+                    Sound.PlaySync();
+                    numSound = numSound.Substring(1);
+                    if (int.Parse(numSound) <= 100)
+                    {
+                        Sound.SoundLocation = @"C:\rsk\sound\en\" + numSound + ".wav";
+                        Sound.PlaySync();
+                    }
+                    else if (int.Parse(numSound) > 100 && int.Parse(numSound) < 1000)
+                    {
+                        char sto = numSound[0];
+                        string stosh = sto + "00";
+                        Sound.SoundLocation = @"C:\rsk\sound\en\" + stosh + ".wav";
+                        Sound.PlaySync();
+
+                        if (numSound[1] == '0')
+                        {
+                            Sound.SoundLocation = @"C:\rsk\sound\en\" + numSound[2] + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string numSoundSub = numSound.Substring(1);
+                            Sound.SoundLocation = @"C:\rsk\sound\en\" + numSoundSub + ".wav";
+                            Sound.PlaySync();
+                        }
+                    }
+                    else if (int.Parse(numSound) >= 1000 && int.Parse(numSound) < 10000)
+                    {
+                        if (numSound[1] == '0' && numSound[2] == '0' && numSound[3] == '0')
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\en\" + stosh + ".wav";
+                            Sound.PlaySync();
+                        }
+                        else
+                        {
+                            string w = numSound.Substring(1);
+                            char tish = numSound[0];
+                            string stosh = tish + "000";
+                            Sound.SoundLocation = @"C:\rsk\sound\en\" + stosh + ".wav";
+                            Sound.PlaySync();
+
+
+                            if (numSound[1] == '0' && numSound[2] == '0')
+                            {
+                                Sound.SoundLocation = @"C:\rsk\sound\en\" + numSound[3] + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else if (numSound[1] == '0' && numSound[2] != '0')
+                            {
+                                string stos = numSound.Substring(2);
+                                Sound.SoundLocation = @"C:\rsk\sound\en\" + stos + ".wav";
+                                Sound.PlaySync();
+                            }
+                            else
+                            {
+                                char sto = numSound[1];
+                                string stoshi = sto + "00";
+                                Sound.SoundLocation = @"C:\rsk\sound\en\" + stoshi + ".wav";
+                                Sound.PlaySync();
+                                if (numSound[2] == '0')
+                                {
+                                    Sound.SoundLocation = @"C:\rsk\sound\en\" + numSound[3] + ".wav";
+                                    Sound.PlaySync();
+                                }
+                                else
+                                {
+                                    string numSoundSub = numSound.Substring(2);
+                                    Sound.SoundLocation = @"C:\rsk\sound\en\" + numSoundSub + ".wav";
+                                    Sound.PlaySync();
+                                }
+                            }
+                        }
+                    }
+                    Sound.SoundLocation = @"C:\rsk\sound\en\Сиздин талон жараксыз.wav";
+                    Sound.PlaySync();
+                    Sound.SoundLocation = @"C:\rsk\sound\en\" + operSound + ".wav";
+                    Sound.PlaySync();
+                }
+                else
+                {
+                    SoundPlayer Sound = new SoundPlayer(@"C:\rsk\sound\kg\А.wav");
+                    Sound.PlaySync();
+                }
+            }
+            catch 
+            {
+            
+            }
+        }
+    }
+}
